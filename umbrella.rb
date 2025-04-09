3269208da09b51b060d7a7f398225c63b40d25e0
yzemlyak <yzemlyak@gmail.com>
2025-04-09 01:50:55 +0000

    Halfway done

--- umbrella.rb
+++ umbrella.rb
@@ -1,3 +1,17 @@
 # Write your solution below!
+require "http"
 require "dotenv/load"
-
+pp "=========================================="
+pp "Will you need an umbrella today?"
+pp "=========================================="
+pp "Where are you today?"
+# user_location = gets.chomp
+user_location = "Gleacher Center, Chicago"
+pp user_location
+env_fetch = ENV.fetch("GMAPS_KEY")
+google_api_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_location}&key=#{env_fetch}"
+pp google_api_url
+resp = HTTP.get(google_api_url)
+raw_body = resp.to_s
+parsed_body = JSON.parse(raw_body)
+pp c = parsed_body.fetch("location")

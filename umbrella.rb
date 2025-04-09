532109f092777ed2be27eb5d2cbd80e72b74123b
yzemlyak <yzemlyak@gmail.com>
2025-04-09 02:13:59 +0000

    Close to the end

--- umbrella.rb
+++ umbrella.rb
@@ -8,8 +8,8 @@ pp "Where are you today?"
 # user_location = gets.chomp
 user_location = "Gleacher Center, Chicago"
 pp user_location
-env_fetch = ENV.fetch("GMAPS_KEY")
-google_api_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_location}&key=#{env_fetch}"
+env_fetch_gmaps = ENV.fetch("GMAPS_KEY")
+google_api_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_location}&key=#{env_fetch_gmaps}"
 resp = HTTP.get(google_api_url)
 raw_body = resp.to_s
 parsed_body = JSON.parse(raw_body)
@@ -17,3 +17,17 @@ lat = parsed_body.fetch("results").at(0).fetch("geometry").fetch("location").fet
 lng = parsed_body.fetch("results").at(0).fetch("geometry").fetch("location").fetch("lng")
 pp lat
 pp lng
+env_fetch_pirate = ENV.fetch("PIRATE_WEATHER_KEY")
+pirate_api_url = "https://api.pirateweather.net/forecast/#{env_fetch_pirate}/#{lat},#{lng}"
+pp pirate_api_url
+resp_pirate = HTTP.get(pirate_api_url)
+raw_pirate = resp_pirate.to_s
+parsed_pirate = JSON.parse(raw_pirate)
+# pp parsed_pirate
+odds_rain = parsed_pirate.fetch("currently").fetch("precipProbability")
+# pp odds_rain
+if odds_rain >= 0.25
+  pp "Bring an umbrella, or you'll be wet!"
+else
+  pp "You're dry... for now!"
+end 
